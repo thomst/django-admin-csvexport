ca0674e90fcf543c3e746724b5edd8e07015abae
Thomas Leichtfuß <leichtfuss@systopia.de>
2020-02-25 19:24:53 +0100

    wrote more tests

--- tests/testapp/tests/test_export.py
+++ tests/testapp/tests/test_export.py
@@ -34,16 +34,30 @@ class ExportTest(TestCase):
         self.post_data['doublequote'] = 'off'
         self.post_data['_selected_action'] = [i for i in range(1,6)]
 
-        self.post_data['ModelA'] = list()
+    def select_fields(self, post_data):
+        post_data['ModelA'] = list()
         for field in get_fields(ModelA):
-            self.post_data['ModelA'].append(field.name)
+            post_data['ModelA'].append(field.name)
         for rel_field in get_rel_fields(ModelA):
             model = rel_field.related_model
             fields = get_fields(model)
-            self.post_data[model.__name__] = list()
+            post_data[model.__name__] = list()
             for field in get_fields(model):
                 ref = '{}.{}'.format(rel_field.name, field.name)
-                self.post_data[model.__name__].append(ref)
+                post_data[model.__name__].append(ref)
+
+    def check_content(self, content, post_data):
+        # test default-values
+        self.assertIn(BYTE_STRING, content)
+        self.assertIn(UNICODE_STRING, content.decode('utf-8'))
+
+        # test header
+        header_a = ','.join(post_data['ModelA'])
+        self.assertIn(header_a, content.decode('utf-8'))
+        header_b = ','.join(post_data['ModelB'])
+        self.assertIn(header_b, content.decode('utf-8'))
+        header_c = ','.join(post_data['ModelC'])
+        self.assertIn(header_c, content.decode('utf-8'))
 
     def test_01_form(self):
         post_data = dict()
@@ -66,39 +80,27 @@ class ExportTest(TestCase):
                 self.assertIn(ref, resp.content.decode('utf-8'))
 
     def test_02_invalid_form(self):
+        # test without any selected field...
         post_data = self.post_data.copy()
         post_data['csvexport_view'] = 'View'
-        del post_data['ModelA']
-        del post_data['ModelB']
-        del post_data['ModelC']
-
-        # test without any selected field...
         resp = self.client.post(self.url, post_data)
-        # self.assertEqual(resp.redirect_chain[0][0], url)
         self.assertEqual(resp.status_code, 200)
         self.assertIn(CSVFieldsForm.ERR_MSG, resp.content.decode('utf-8'))
 
     def test_03_csv_view(self):
         post_data = self.post_data.copy()
         post_data['csvexport_view'] = 'View'
+        self.select_fields(post_data)
         resp = self.client.post(self.url, post_data)
         self.assertEqual(resp.status_code, 200)
-
-        # test default-values
-        self.assertIn(BYTE_STRING, resp.content)
-        self.assertIn(UNICODE_STRING, resp.content.decode('utf-8'))
-
-        # test header
-        header_a = ','.join(post_data['ModelA'])
-        self.assertIn(header_a, resp.content.decode('utf-8'))
-        header_b = ','.join(post_data['ModelB'])
-        self.assertIn(header_b, resp.content.decode('utf-8'))
-        header_c = ','.join(post_data['ModelC'])
-        self.assertIn(header_c, resp.content.decode('utf-8'))
+        self.assertIn("text/plain", resp.get('Content-Type'))
+        self.check_content(resp.content, post_data)
 
     def test_04_csv_download(self):
-        # FIXME: Don't get a download-response or don't know how to test it...
         post_data = self.post_data.copy()
         post_data['csvexport_download'] = 'Download'
+        self.select_fields(post_data)
         resp = self.client.post(self.url, post_data)
+        self.check_content(resp.content, post_data)
         self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.get('Content-Type'), "text/comma-separated-values")
