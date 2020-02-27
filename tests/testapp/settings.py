9a9ec3d095a9db6872754e84e6589e0286fc56ac
Thomas Leichtfuß <leichtfuss@systopia.de>
2020-02-27 17:15:01 +0100

    removed obsolete syspath-inclusion

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import sys
 
-sys.path.append("../..")
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
