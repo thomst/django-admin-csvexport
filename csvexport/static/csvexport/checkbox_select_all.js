172515ca2b7e284a1990705bace3d64e7821e4bf
Thomas Leichtfuß <leichtfuss@systopia.de>
2020-04-18 23:52:21 +0200

    removed debug-statement console.log

--- csvexport/static/csvexport/checkbox_select_all.js
+++ csvexport/static/csvexport/checkbox_select_all.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     inputs = $('input[id$="_select_all"]');
-    console.log(inputs);
     inputs.change(function() {
         options = $('#' + this.id.slice(0,-11)).find('input');
         if (this.checked) {
