51d32959b30c998a76820e56f9afa35cb95ff433
Thomas Leichtfuß <leichtfuss@systopia.de>
2020-04-21 20:26:00 +0200

    Writing tests with and without the format-form

--- tests/testapp/tests/test_export.py
+++ tests/testapp/tests/test_export.py
@@ -1,17 +1,32 @@
 
 
-from django.contrib.auth.models import User
 from django.test import TestCase
+from django.contrib.auth.models import User
 from django.urls import reverse
 
-from csvexport import apps
+from csvexport import apps, settings
 from csvexport.actions import get_fields
 from csvexport.actions import get_rel_fields
-from csvexport.forms import CSVFieldsForm
+from csvexport.forms import CSVFieldsForm, CSVFormatForm
 from ..models import ModelA, ModelB, ModelC, ModelD
 from ..models import UNICODE_STRING
 from ..models import BYTE_STRING
 from ..management.commands.testapp import create_test_data
+
+
+class AlterSettings:
+    def __init__(self, **kwargs):
+        self.settings = kwargs
+        self.origin = dict()
+
+    def __enter__(self):
+        for setting, value in self.settings.items():
+            self.origin[setting] = getattr(settings, setting)
+            setattr(settings, setting, value)
+
+    def __exit__(self, type, value, traceback):
+        for setting, value in self.origin.items():
+            setattr(settings, setting, value)
 
 
 class ExportTest(TestCase):
@@ -41,15 +56,18 @@ class ExportTest(TestCase):
         self.admin = User.objects.get(username='admin')
         self.client.force_login(self.admin)
         self.url = reverse('admin:testapp_modela_changelist')
+
         self.post_data = dict()
         self.post_data['action'] = 'csvexport'
         self.post_data['csvexport'] = 'csvexport'
-        self.post_data['delimiter'] = ''
-        self.post_data['escapechar'] = ''
-        self.post_data['lineterminator'] = ''
-        self.post_data['quotechar'] = ''
-        self.post_data['doublequote'] = 'off'
         self.post_data['_selected_action'] = [i for i in range(1,6)]
+
+        self.csv_format = dict()
+        self.csv_format['delimiter'] = settings.CSV_EXPORT_DELIMITER
+        self.csv_format['escapechar'] = settings.CSV_EXPORT_ESCAPECHAR
+        self.csv_format['lineterminator'] = settings.CSV_EXPORT_LINETERMINATOR
+        self.csv_format['quotechar'] = settings.CSV_EXPORT_QUOTECHAR
+        self.csv_format['doublequote'] = 'off'
 
     def check_content(self, content, post_data):
         # test default-values
@@ -64,12 +82,22 @@ class ExportTest(TestCase):
         post_data = dict()
         post_data['action'] = 'csvexport'
         post_data['_selected_action'] = [i for i in range(1,6)]
+        format_form = CSVFormatForm()
         resp = self.client.post(self.url, post_data)
 
+        # check form with format-form
         self.assertEqual(resp.status_code, 200)
-
+        for field in format_form.fields.keys():
+            self.assertIn(field, resp.content.decode('utf-8'))
         for option in self.options:
             self.assertIn(option, resp.content.decode('utf-8'))
+
+        # check form without format-form
+        with AlterSettings(CSV_EXPORT_FORMAT_FORM=False):
+            resp = self.client.post(self.url, post_data)
+            self.assertEqual(resp.status_code, 200)
+            for field in format_form.fields.keys():
+                self.assertNotIn(field, resp.content.decode('utf-8'))
 
     def test_02_invalid_form(self):
         # test without any selected field...
@@ -83,6 +111,16 @@ class ExportTest(TestCase):
         post_data = self.post_data.copy()
         post_data.update(self.fields)
         post_data['csvexport_view'] = 'View'
+
+        # without csv-format-data
+        with AlterSettings(CSV_EXPORT_FORMAT_FORM=False):
+            resp = self.client.post(self.url, post_data)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("text/plain", resp.get('Content-Type'))
+            self.check_content(resp.content, post_data)
+
+        # with format-data
+        post_data.update(self.csv_format)
         resp = self.client.post(self.url, post_data)
         self.assertEqual(resp.status_code, 200)
         self.assertIn("text/plain", resp.get('Content-Type'))
@@ -92,6 +130,16 @@ class ExportTest(TestCase):
         post_data = self.post_data.copy()
         post_data.update(self.fields)
         post_data['csvexport_download'] = 'Download'
+
+        # without csv-format-data
+        with AlterSettings(CSV_EXPORT_FORMAT_FORM=False):
+            resp = self.client.post(self.url, post_data)
+            self.check_content(resp.content, post_data)
+            self.assertEqual(resp.status_code, 200)
+            self.assertEqual(resp.get('Content-Type'), "text/comma-separated-values")
+
+        # with format-data
+        post_data.update(self.csv_format)
         resp = self.client.post(self.url, post_data)
         self.check_content(resp.content, post_data)
         self.assertEqual(resp.status_code, 200)
