2946d32eb0fa6fe1de1cf30db48c29f9dfdeae64
Joseph Coco <coco.joseph@gmail.com>
2024-05-20 12:19:17 -0500

    Forgot to include cached_property import

--- csvexport/actions.py
+++ csvexport/actions.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render
 from django import forms
+from functools import cached_property
 
 from csvexport import settings
 from .forms import CSVFormatForm
